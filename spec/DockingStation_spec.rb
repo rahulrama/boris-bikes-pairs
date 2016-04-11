8026de85cdb07f70a0d797fc9b6884d0250f4b18
ollieh-m <ollie.hm@cantab.net>
2016-04-11 17:31:05 +0100

    Challenge 8 - make test pass by defining release_bike method

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,8 +2,7 @@ require 'DockingStation'
 
 describe DockingStation do
     it 'responds to release_bike' do
-        expect(DockingStation.new).to respond_to release_bike
-    
-     end
+        expect(DockingStation.new).to respond_to 'release_bike'
     end
+end
  
