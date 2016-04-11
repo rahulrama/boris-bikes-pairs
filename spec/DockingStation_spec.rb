4af51534a2f4fe095d94311e2a642cbf6a1861af
RichardWatkins1 <richardwatkins1@gmail.com>
2016-04-11 17:24:10 +0100

    Challenge 8 - expect DockingStation.new to respond_to release_bike

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,9 @@
 require 'DockingStation'
 
 describe DockingStation do
+    it 'responds to release_bike' do
+        expect(DockingStation.new).to respond_to release_bike
     
-end
+     end
+    end
  
