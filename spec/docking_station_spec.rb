ec8e68d00c21408069eeec063d5e5b9f39b8b0f9
Jackrim1 <jriminton@gmail.com>
2016-04-12 12:28:56 +0100

    Jack - Step11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,12 @@
 load 'docking_station.rb'
 
 
+
 describe DockingStation do
 
 it { is_expected.to respond_to :release_bike }
+
+it { is_expected.to respond_to(:dock).with(1).argument }
 
 it "releases working bikes" do
 	bike = subject.release_bike
