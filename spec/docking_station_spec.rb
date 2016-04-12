b58030928cf54d0be2f07cea252af490d5b7a597
Aday Mesa <adaymesagc@gmail.com>
2016-04-12 15:03:31 +0100

    chall.12 - Aday

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,6 +10,9 @@ it { is_expected.to respond_to(:dock).with(1).argument }
 
 it { is_expected.to respond_to (:bike) }
 
+                                                                                                                                                                                                                                                                                                                                                     
+
+
 it 'docks something' do
   bike = Bike.new
   expect(subject.dock(bike)).to eq bike
@@ -27,5 +30,12 @@ it 'returns docked bikes' do
   expect(subject.bike).to eq bike
 end
 
-
+describe "#release_bike" do
+	it "releases a bike" do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.release_bike).to eq bike
+	end
 end
+end
+
