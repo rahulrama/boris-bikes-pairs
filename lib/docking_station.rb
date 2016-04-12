146fe645e9ea2177ef7d04a07c9006a660aed052
Max Provin <maxprovin@outlook.com>
2016-04-12 18:17:43 +0100

    step 15 - single repository principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,19 +9,28 @@ class DockingStation
   end
 
   def dock(bike)
-    raise 'Docking station full' if @bikes.length >= 20
+    raise 'Docking station full' if full?
     @bikes << bike
     @bikes.last
 
   end
 
   def release_bike
-    raise 'No bikes available' if @bikes.empty?
+    raise 'No bikes available' if empty?
     @bikes.pop
   end
 
   def bikes
     @bikes.last
+  end
+
+  private
+  def full?
+    @bikes.length >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
