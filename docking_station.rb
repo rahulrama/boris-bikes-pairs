9c842069f3fe1550711c131cb31fd09ef4512a27
Aday Mesa <adaymesagc@gmail.com>
2016-04-12 15:32:42 +0100

    chall.3 completed - Aday

--- docking_station.rb
+++ docking_station.rb
@@ -2,14 +2,13 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike
-
   def release_bike
 		fail 'No bikes available' unless @bike
     @bike
 	end
 
 	def dock(bike)
+    fail "Docking station full " if @bike
     @bike = bike
 	end
 
