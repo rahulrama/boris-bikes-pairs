71d18529959a44c060ff30ebaf24d0544f5ff4e6
Jackrim1 <jriminton@gmail.com>
2016-04-12 16:05:35 +0100

    Jack 14 stuck on complex attributes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,7 +8,7 @@ it { is_expected.to respond_to :release_bike }
 
 it { is_expected.to respond_to(:dock).with(1).argument }
 
-it { is_expected.to respond_to (:bike) }
+it { is_expected.to respond_to (:bikes) }
 
                                                                                                                                                                                                                              ''
 
@@ -31,11 +31,11 @@ describe '#release_bike' do
 	end
 
 describe "#dock" do
-	it "raises an error when full" do 
-		subject.dock(Bike.new)
+	it "raises an error when full" do
+		20.times { subject.dock Bike.new }
 		expect { subject.dock Bike.new }.to raise_error "Docking station full"
 end
 end
-
+end
 end
 
