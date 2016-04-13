acf935101953f874b862acbba9872f5ebe856929
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-13 19:46:01 +0200

    Step 19: Isolating tests with doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,66 +2,63 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  let(:bike) { Bike.new }
+  #let(:bike) { Bike.new }
     let(:ds) { DockingStation.new }
     let(:test_bikes) { ds.send(:bikes) } #To access private instance variable bikes
 
 
-  it { is_expected.to respond_to(:release_bike) }
-  it 'does not release a broken bike' do
-      test_bikes << bike
-      bike.report_status(false)
+  it { is_expected.to respond_to(:release_bike) } #Fine
 
+  it 'does not release a broken bike' do #Dependency
+      double_bike = double(:bike)
+      test_bikes << double_bike
+     double_bike.report_status(false)
       expect { ds.release_bike }.to raise_error "It's broken" unless test_bikes.last.working
     end
 
-  it 'releases a new bike that is also working' do
-   test_bikes << bike
+  it 'releases a new bike that is also working' do #Dependency
+   test_bikes << double(:bike)
    (expect(ds.release_bike.class).to eq Bike)
-   test_bikes << bike
+   test_bikes << double(:bike)
    (expect(ds.release_bike.working).to eq true)
   end
 
   it '#returns the bike' do
-  (expect(ds.dock(bike)).to eq test_bikes)
+  (expect(ds.dock(double(:bike))).to eq test_bikes) #Fine
   end
 
+  #fine
   it 'expects an error when docking_station tries to instantiate a new bike if there are 0 bikes in the dock' do
       expect { ds.release_bike }.to raise_exception if test_bikes.empty?
   end
 
+  #fine
   it 'expects an error when dock tries to add a bike when subject bike_rack is greater than capacity' do
       expect { ds.dock }.to raise_exception if test_bikes.count >= subject.capacity
   end
 
+  #Dependency
   it 'raises an error when full' do
-   subject.capacity.times { subject.dock bike }
-   expect { subject.dock bike }.to raise_exception if test_bikes.count >= subject.capacity
+   subject.capacity.times { subject.dock double(:bike) }
+   expect { subject.dock double(:bike) }.to raise_exception if test_bikes.count >= subject.capacity
  end
 
-  # it 'sets the capacity of dock when the user does not want the default capacity' do
-  #   expect { subject.capacity(30) }.to eq subject.capacity
-  # end
-
+  #Fine
   it 'makes sure capacity is set to DEFAULT_CAPACITY when no parameters are passed to DockingStation.new' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
+  #Dependency
   describe 'initialization' do #no 17
     it 'has a variable capacity' do
-      ds.capacity.times { ds.dock bike }
-      expect{ ds.dock bike }.to raise_exception "I'm all full up!"
+      ds.capacity.times { ds.dock double(:bike) }
+      expect{ ds.dock double(:bike) }.to raise_exception "I'm all full up!"
     end
-
+  #Dependency
     it 'has a default capacity' do
-      DockingStation::DEFAULT_CAPACITY.times { ds.dock bike }
-      expect{ ds.dock bike }.to raise_exception "I'm all full up!"
+      DockingStation::DEFAULT_CAPACITY.times { ds.dock double(:bike) }
+      expect{ ds.dock double(:bike) }.to raise_exception "I'm all full up!"
     end
   end
 
 end
-
-
-#As a member of the public
-#So I can return bikes I've hired
-#I want to dock my bike at the docking station
