e6ef0928e452a5376a171ed6cd6da649b5c93acc
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-13 20:54:37 +0200

    Step 20: Mocking behaviour on doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,29 +2,29 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  #let(:bike) { Bike.new }
+    let(:bike) { double(:bike, working: true) }
     let(:ds) { DockingStation.new }
     let(:test_bikes) { ds.send(:bikes) } #To access private instance variable bikes
-
 
   it { is_expected.to respond_to(:release_bike) } #Fine
 
     it 'does not release a broken bike' do #Dependency
-        double_bike = double(:bike)
-        test_bikes << double_bike
-       double_bike.report_status(false)
+        test_bikes << bike
+        allow(bike).to receive(:report_status).with(false).and_return(false) #To pass with argument
+        bike.report_status(false)
+        allow(bike).to receive(:working).and_return(true)
         expect { ds.release_bike }.to raise_error "It's broken" unless test_bikes.last.working
       end
 
     it 'releases a new bike that is also working' do #Dependency
-     test_bikes << double(:bike)
-     (expect(ds.release_bike.class).to eq Bike)
-     test_bikes << double(:bike)
+     test_bikes << bike
+#      bike = double(:bike, working: true)
+     #allow(bike).to receive(:working).and_return(true)
      (expect(ds.release_bike.working).to eq true)
     end
 
   it '#returns the bike' do
-  (expect(ds.dock(double(:bike))).to eq test_bikes) #Fine
+  (expect(ds.dock(bike)).to eq test_bikes) #Fine
   end
 
   #fine
@@ -39,8 +39,8 @@ describe DockingStation do
 
   #Dependency
   it 'raises an error when full' do
-   subject.capacity.times { subject.dock double(:bike) }
-   expect { subject.dock double(:bike) }.to raise_exception if test_bikes.count >= subject.capacity
+   subject.capacity.times { subject.dock bike }
+   expect { subject.dock bike }.to raise_exception if test_bikes.count >= subject.capacity
  end
 
   #Fine
@@ -51,13 +51,13 @@ describe DockingStation do
   #Dependency
   describe 'initialization' do #no 17
     it 'has a variable capacity' do
-      ds.capacity.times { ds.dock double(:bike) }
-      expect{ ds.dock double(:bike) }.to raise_exception "I'm all full up!"
+      ds.capacity.times { ds.dock bike }
+      expect{ ds.dock bike }.to raise_exception "I'm all full up!"
     end
   #Dependency
     it 'has a default capacity' do
-      DockingStation::DEFAULT_CAPACITY.times { ds.dock double(:bike) }
-      expect{ ds.dock double(:bike) }.to raise_exception "I'm all full up!"
+      DockingStation::DEFAULT_CAPACITY.times { ds.dock bike }
+      expect{ ds.dock bike }.to raise_exception "I'm all full up!"
     end
   end
 
