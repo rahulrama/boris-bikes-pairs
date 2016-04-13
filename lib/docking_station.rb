bb2c106d39daf86aec303cc4e2997e0351086a8f
Nick <nm.richards@yahoo.com>
2016-04-13 10:11:52 -0400

    Challenge 16 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,7 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bikes
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @bikes = []
@@ -22,7 +23,7 @@ class DockingStation
   private
 
   def full?
-      @bikes.count >= 20
+      @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
