e6484884170630d48bb9b000475a16388460ff7b
ollieh-m <ollie.hm@cantab.net>
2016-04-14 17:09:31 +0100

    Challenge 21 - create collect_broken method

--- lib/Van.rb
+++ lib/Van.rb
@@ -1,2 +1,15 @@
+require_relative 'DockingStation'
+
 class Van
+
+	attr_reader :bikes
+
+	def initialize
+		@bikes = []
+	end
+
+	def collect_broken(station)
+		@bikes = station.bikes.select{|bike| bike.broken?}
+	end
+
 end
